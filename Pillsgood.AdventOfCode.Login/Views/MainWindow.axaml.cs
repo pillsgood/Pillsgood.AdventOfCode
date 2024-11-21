838755d6d83227db6e5928fbc10abefb04e01570
Pillsgood <Arashm35@gmail.com>
2024-11-21 19:26:52 +0800

    Refactor MainWindow class declaration
    Changed the MainWindow class from 'internal class' to 'internal partial class' to allow its implementation to be split across multiple files. This modification enhances maintainability and scalability of the code by supporting partial classes.

--- Pillsgood.AdventOfCode.Login/Views/MainWindow.axaml.cs
+++ Pillsgood.AdventOfCode.Login/Views/MainWindow.axaml.cs
@@ -5,7 +5,7 @@ using Pillsgood.AdventOfCode.Login.ViewModels;
 
 namespace Pillsgood.AdventOfCode.Login.Views;
 
-internal class MainWindow : ReactiveWindow<MainViewModel>
+internal partial class MainWindow : ReactiveWindow<MainViewModel>
 {
     public MainWindow()
     {
